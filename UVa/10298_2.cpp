25d10ba41e7b9e3abc1fab54b6a80a9275d81a74
Saya <sa0726866@gmail.com>
2019-05-28 11:34:18 +0800

    Update 10298_2.cpp

--- UVa/10298_2.cpp
+++ UVa/10298_2.cpp
@@ -7,6 +7,8 @@ char s[1048576];
 int main()
 {
 	int n, i, j, ans;
+	//freopen("d:/apcs/in.txt", "r", stdin);
+	//freopen("d:/apcs/out.txt", "w", stdout);
 	while (fgets(s, sizeof(s), stdin))
 	{
 		if (!strtok(s, "\r\n"))
@@ -34,14 +36,15 @@ int main()
 		n = i;
 		jmp[n] = j;
 		ans = -1;
-		while (i > ans)
+		while (i > 0 && i != ans)
 		{
 			if (ans != i-jmp[i])
 			{
-				ans = i - jmp[i];
+				ans = n - jmp[i];
 			}
 			i = jmp[i];
 		}
+		//printf("ans: %d\n", ans);
 		printf("%d\n", n/ans);
 	}
 	return 0;
