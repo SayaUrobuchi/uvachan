e2651a51d5c81ef992f5eed8f3fd82671b749cee
Saya <sa0726866@gmail.com>
2020-03-04 17:00:27 +0800

    Update dp_t.cpp

--- AtCoder/dp_t.cpp
+++ AtCoder/dp_t.cpp
@@ -1,29 +1,35 @@
 #include<iostream>
 using namespace std;
 
+const int M = 1000000007;
+
 int dp[4096][4096];
 char s[1048576];
 
 int main() {
 	int n, i, j;
 	scanf("%d%s", &n, s);
-	dp[n][0] = 1;
-	for (i=n-1; i>=0; i--)
+	for (i=0; i<n; i++)
+	{
+		dp[n-1][i] = i+1;
+	}
+	for (i=n-2; i>=0; i--)
 	{
 		for (j=0; j<=i; j++)
 		{
-			if (s[n-i] == '>')
+			if (s[n-i-2] == '>')
 			{
-				dp[i][j] = (dp[i-1][i-1] - dp[i-1][j] + M) % M;
+				dp[i][j] = (dp[i+1][i+1] - dp[i+1][j] + M) % M;
 			}
 			else
 			{
-				dp[i][j] = dp[i-1][i-j];
+				dp[i][j] = dp[i+1][j];
 			}
 			if (j)
 			{
 				dp[i][j] = (dp[i][j] + dp[i][j-1]) % M;
 			}
+			//printf("%d %d: %d\n", i, j, dp[i][j]);
 		}
 	}
 	printf("%d\n", dp[0][0]);
